ff413563b83f2c72bd61c7d09477116c56669e15
Kain (Pseudonym) <88363187+EKQRCalamity@users.noreply.github.com>
2024-05-16 20:27:37 +0200

    Update, read desc
    Added <esc> as close key
    Added ability to choose light and dark bg mode
    Added display for keymaps
    Set title and color buf non modifiable
    Changed save location for persisting theme file

--- lua/wardrobe/window.lua
+++ lua/wardrobe/window.lua
@@ -8,21 +8,43 @@ local function vim_colorscheme(colorscheme)
   vim.cmd('colorscheme ' .. colorscheme)
 end
 
-local function apply_colorscheme(colorschemes, closewindow, main_win, title_win, preview_win)
+local function vim_background(background)
+  vim.cmd("set background=" .. background)
+end
+
+local function split(str, sep)
+  local ret = {}
+
+  for s in string.gmatch(str, "([^"..sep.."]+)") do
+    table.insert(ret, s)
+  end
+
+  return ret
+end
+
+local function apply_colorscheme(wincontent, closewindow, main_win, title_win, preview_win)
   local current_line = vim.fn.line('.')
-  local colorscheme = colorschemes[current_line]
-  if colorscheme then
-    vim_colorscheme(colorscheme)
-    if closewindow then
-      WINDOW.save_theme(colorscheme)
-      if main_win then
-        vim.api.nvim_win_close(main_win, true)
-      end
-      if title_win then
-        vim.api.nvim_win_close(title_win, true)
-      end
-      if preview_win then
-        vim.api.nvim_win_close(preview_win, true)
+  local content = wincontent[current_line]
+  if content then
+
+    if content == "dark bg" then
+      vim_background("dark")
+    elseif content == "light bg" then
+      vim_background("light")
+    else
+      content = split(content, " ")[2]
+      vim_colorscheme(content)
+      if closewindow then
+        WINDOW.save_theme(content)
+        if main_win then
+          vim.api.nvim_win_close(main_win, true)
+        end
+        if title_win then
+          vim.api.nvim_win_close(title_win, true)
+        end
+        if preview_win then
+          vim.api.nvim_win_close(preview_win, true)
+        end
       end
     end
   end
@@ -36,16 +58,28 @@ local function set_keymap(buf, key, func)
   })
 end
 
+local function close(window)
+  if window then
+    vim.api.nvim_win_close(window, true)
+  end
+end
+
+local function close_all(window1, window2, window3)
+  close(window1)
+  close(window2)
+  close(window3)
+end
+
 WINDOW.save_theme = function (name)
-  local config_dir = vim.fn.stdpath('config')
-  local config_file = config_dir .. '/wardrobe-nvim-theme'
+  local config_dir = vim.fn.stdpath('data')
+  local config_file = config_dir .. '\\wardrobe-nvim-theme.chosen'
 
   vim.fn.writefile({name}, config_file)
 end
 
 WINDOW.load_theme = function ()
-  local config_dir = vim.fn.stdpath('config')
-  local config_file = config_dir .. '/wardrobe-nvim-theme'
+  local config_dir = vim.fn.stdpath('data')
+  local config_file = config_dir .. '\\wardrobe-nvim-theme.chosen'
 
   if vim.fn.filereadable(config_file) == 1 then
     local data = vim.fn.readfile(config_file)
@@ -62,7 +96,6 @@ WINDOW.open_preview_window = function()
   if vim.o.columns < min_term_width then
     return nil
   end
-
 end
 
 WINDOW.open_window = function()
@@ -71,10 +104,10 @@ WINDOW.open_window = function()
   local title_buf = vim.api.nvim_create_buf(false, true)
   local preview_buf = vim.api.nvim_create_buf(false, true)
   local width = 50
-  local title_width = 50
-  local main_win_width = 50
+  local title_width = 60
+  local main_win_width = 60
   if preview then
-    main_win_width = 20
+    main_win_width = 22
   end
   if preview then
     title_width = 86
@@ -92,14 +125,17 @@ WINDOW.open_window = function()
     style = 'minimal',
     relative = 'editor',
     width = title_width,
-    height = 1,
+    height = 2,
     col = col,
     row = row-4,
     border = 'double'
   }
 
   local title_window = vim.api.nvim_open_win(title_buf, false, title_options)
-  vim.api.nvim_buf_set_lines(title_buf, 0, -1, false, {"Wardrobe - A small theme chooser for lazy people!"})
+  vim.api.nvim_buf_set_lines(title_buf, 0, -1, false, {
+    "Wardrobe - A small theme chooser for lazy people!",
+    "• q&<esc> » Exit • p » Preview • <CR> » Save (& Close)"
+  })
 
   local preview_win = nil
   if preview then
@@ -141,25 +177,56 @@ WINDOW.open_window = function()
   }
 
   local window = vim.api.nvim_open_win(buf, true, options)
-
   local colorschemes = get_colorschemes()
+  local modes = {"dark bg", "light bg"}
+  local main_win_content = {}
 
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, colorschemes)
+  -- Append modes to main_win_content
+  for _, value in ipairs(modes) do
+    table.insert(main_win_content, value)
+  end
+
+  -- Append colorschemes to main_win_content
+  for i, value in ipairs(colorschemes) do
+    table.insert(main_win_content, i .. " " .. value)
+  end
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, main_win_content)
+
+  vim.api.nvim_buf_set_option(title_buf, 'modifiable', false)
+  vim.api.nvim_buf_set_option(buf, 'modifiable', false)
 
   set_keymap(buf, "<CR>", function()
-    apply_colorscheme(colorschemes, true, window, title_window, preview_win)
+    apply_colorscheme(main_win_content, true, window, title_window, preview_win)
   end)
 
   set_keymap(buf, "p", function()
-    apply_colorscheme(colorschemes, false, window, title_window, preview_win)
+    apply_colorscheme(main_win_content, false, window, title_window, preview_win)
+  end)
+
+  set_keymap(buf, "<esc>", function()
+    close_all(window, title_window, preview_win)
   end)
 
   set_keymap(buf, "q", function()
-    vim.api.nvim_win_close(window, true)
-    vim.api.nvim_win_close(title_window, true)
-    if preview_win then
-      vim.api.nvim_win_close(preview_win, true)
-    end
+    close_all(window, title_window, preview_win)
+  end)
+
+  set_keymap(preview_buf, "<esc>", function()
+    close_all(window, title_window, preview_win)
+  end)
+
+  set_keymap(preview_buf, "q", function()
+    close_all(window, title_window, preview_win)
+  end)
+
+
+  set_keymap(title_buf, "<esc>", function()
+    close_all(window, title_window, preview_win)
+  end)
+
+  set_keymap(title_buf, "q", function()
+    close_all(window, title_window, preview_win)
   end)
 end
 
